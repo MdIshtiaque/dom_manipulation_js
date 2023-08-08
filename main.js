49e1aa13496d1f747490b0bd5844a08d5470c216
Md___Emon <ishtiaqueferdous@nextgenitltd.com>
2023-08-08 10:05:27 +0600

    style modify by dom

--- main.js
+++ main.js
@@ -3,21 +3,21 @@ let val = document.getElementById('task_list_title');
 console.log(val);
 
 /* Changing Style */
-val.style.backgroundColor = '#ef4444';
-val.style.color = '#fff';
+// val.style.backgroundColor = '#ef4444';
+val.style.color = 'black';
 val.style.fontSize = '20px';
 val.style.fontWeight = 'bold';
 val.style.textAlign = 'center';
 val.style.padding = '10px';
 val.style.border = '1px solid #000000 #ddd';
 val.style.borderRadius = '10px';
-val.style.boxShadow = '0 0 3px #000000';
+val.style.boxShadow = '0 0 1px #000000';
 
 /* Chnaging Content */
 val.textContent = 'Task Collection';
 
 /** multiple element by query selector */
-document.querySelectorAll('.task-item').style.backgroundColor = '#ef4444';
+// document.querySelector('.task-item').style.backgroundColor = '#ef4444';
 // console.log(multiple);
 
 // let doc;
@@ -32,3 +32,21 @@ document.querySelectorAll('.task-item').style.backgroundColor = '#ef4444';
 // });
 
 // console.log(doc);
+
+
+let tasks = document.getElementsByClassName('task-item');
+
+console.log(tasks);
+
+
+let list = document.querySelectorAll('.task-item');
+list.forEach((element, index) => {
+  console.log(element.getElementsByClassName, element, index);
+  // element.textContent = "hello, this is task no "+ index;
+  element.textContent = `${index + 1}. Hello, I am a task.`; // object literal
+  element.style.margin = '2px';
+  element.style.backgroundColor = '#99f6e4';
+  element.style.borderRadius = '6px';
+  element.style.padding = '10px';
+});
+console.log(list);
